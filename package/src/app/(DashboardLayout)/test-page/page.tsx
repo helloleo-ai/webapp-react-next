ce79ec7c9340d7841e84651f2e940847d2429c27
terros <augustin@terros.io>
2024-06-21 00:23:31 +0200

    feat: added `TestPage` component with MUI and custom containers

---
+++ package/src/app/(DashboardLayout)/test-page/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+import { Typography } from '@mui/material';
+import PageContainer from '@/app/(DashboardLayout)/components/container/PageContainer';
+import DashboardCard from '@/app/(DashboardLayout)/components/shared/DashboardCard';
+
+const TestPage = () => {
+  return (
+    <PageContainer title="Test Page" description="this is a Test page">
+      <DashboardCard title="Test Page">
+        <Typography>This is a test page</Typography>
+      </DashboardCard>
+    </PageContainer>
+  );
+};
+
+export default TestPage;
