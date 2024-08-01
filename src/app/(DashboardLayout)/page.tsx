b4571d9236a0ff7fe6aeece27fe612ec7ac35bc2
Hello Leo <dev+helloleo@terros.io>
2024-08-01 16:21:36 +0000

    chore: remove OlympicMedalsGraph component from dashboard layout

--- src/app/(DashboardLayout)/page.tsx
+++ src/app/(DashboardLayout)/page.tsx
@@ -1,14 +1,13 @@
 'use client'
 import { Typography, Grid, Box, Tooltip, Button } from '@mui/material';
+import { Grid, Box } from '@mui/material';
 import PageContainer from '@/app/(DashboardLayout)/components/container/PageContainer';
 import SalesOverview from '@/app/(DashboardLayout)/components/dashboard/SalesOverview';
-import { useState } from 'react';
 import YearlyBreakup from '@/app/(DashboardLayout)/components/dashboard/YearlyBreakup';
 import RecentTransactions from '@/app/(DashboardLayout)/components/dashboard/RecentTransactions';
 import ProductPerformance from '@/app/(DashboardLayout)/components/dashboard/ProductPerformance';
 import Blog from '@/app/(DashboardLayout)/components/dashboard/Blog';
 import MonthlyEarnings from '@/app/(DashboardLayout)/components/dashboard/MonthlyEarnings';
-import ChartModal from '@/app/(DashboardLayout)/components/dashboard/ChartModal';
 
 const Dashboard = () => {
   const [showSalesOverview, setShowSalesOverview] = useState(true);
