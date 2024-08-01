8ef4d63dd5faf38c646b239d095b174f2d403f6f
Terros <augustin@terros.io>
2024-08-01 18:18:19 +0200

    fix error

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/webapp-react-next-325b9104-f675-4129-9123-e1d436c6147c',
-  images: {
-    unoptimized: true,
-  },
   reactStrictMode: true,
 };
 
