c656599fceda9d5c0202dfd24b3c429e69f3f00b
Hello Leo <dev+helloleo@terros.io>
2024-08-02 12:55:28 +0000

    chore: refactor Header component and remove "Upgrade to Pro" button

--- src/app/(DashboardLayout)/layout/header/Header.tsx
+++ src/app/(DashboardLayout)/layout/header/Header.tsx
@@ -3,36 +3,34 @@ import { Box, AppBar, Toolbar, styled, Stack, IconButton, Badge, Button } from '
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 // components
-import React from "react";
-import { Box, AppBar, Toolbar, styled, Stack, IconButton, Badge, Button } from "@mui/material";
-import PropTypes from "prop-types";
+import React from 'react';
+import { Box, AppBar, Toolbar, styled, Stack, IconButton, Badge } from '@mui/material';
+import PropTypes from 'prop-types';
 
 // components
-import Profile from "./Profile";
-import { IconBellRinging, IconMenu, IconMoon, IconSun } from "@tabler/icons-react";
-import { useTheme } from "@/utils/theme/ThemeContext";
+import Profile from './Profile';
+import { IconBellRinging, IconMenu } from '@tabler/icons-react';
 
 interface ItemType {
   toggleMobileSidebar: (event: React.MouseEvent<HTMLElement>) => void;
 }
 
+const AppBarStyled = styled(AppBar)(({ theme }) => ({
+  boxShadow: 'none',
+  background: theme.palette.background.paper,
+  justifyContent: 'center',
+  backdropFilter: 'blur(4px)',
+  [theme.breakpoints.up('lg')]: {
+    minHeight: '70px',
+  },
+}));
+
+const ToolbarStyled = styled(Toolbar)(({ theme }) => ({
+  width: '100%',
+  color: theme.palette.text.secondary,
+}));
+
 const Header = ({ toggleMobileSidebar }: ItemType) => {
-  const { isDarkMode, toggleTheme } = useTheme();
-
-  const AppBarStyled = styled(AppBar)(({ theme }) => ({
-    boxShadow: "none",
-    background: theme.palette.background.paper,
-    justifyContent: "center",
-    backdropFilter: "blur(4px)",
-    [theme.breakpoints.up("lg")]: {
-      minHeight: "70px",
-    },
-  }));
-  const ToolbarStyled = styled(Toolbar)(({ theme }) => ({
-    width: "100%",
-    color: theme.palette.text.secondary,
-  }));
-
   return (
     <AppBarStyled position="sticky" color="default">
       <ToolbarStyled>
@@ -52,10 +50,8 @@ const Header = ({ toggleMobileSidebar }: ItemType) => {
 
         <IconButton
           size="large"
-          aria-label="show 11 new notifications"
+          aria-label="show notifications"
           color="inherit"
-          aria-controls="msgs-menu"
-          aria-haspopup="true"
         >
           <Badge variant="dot" color="primary">
             <IconBellRinging size="21" stroke="1.5" />
@@ -63,12 +59,6 @@ const Header = ({ toggleMobileSidebar }: ItemType) => {
         </IconButton>
         <Box flexGrow={1} />
         <Stack spacing={1} direction="row" alignItems="center">
-          <IconButton onClick={toggleTheme} color="inherit">
-            {isDarkMode ? <IconSun size="21" /> : <IconMoon size="21" />}
-          </IconButton>
-          <Button variant="contained" disableElevation color="primary" target="_blank" href="https://adminmart.com/product/modernize-next-js-admin-dashboard/?aff=codedthemes">
-            Upgrade to Pro
-          </Button>
           <Profile />
         </Stack>
       </ToolbarStyled>
@@ -77,7 +67,7 @@ const Header = ({ toggleMobileSidebar }: ItemType) => {
 };
 
 Header.propTypes = {
-  sx: PropTypes.object,
+  toggleMobileSidebar: PropTypes.func,
 };
 
 export default Header;
